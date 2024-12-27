f8bfcf1e37b45a0fd7d3754aac08c577af709463
Noah Steiniger <noah.steiniger@epitech.eu>
2024-12-27 17:00:43 +0100

    feat (menu): background + effect

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -3,35 +3,96 @@
 import Link from 'next/link';
 import React from 'react';
 import {gsap} from 'gsap';
+import "./menuStyle.css";
+
 
 const I = ({children}: {children: React.ReactNode}) => <span className="font-[family-name:var(--font-ppuli)] font-extralight text-xl ">{children}</span>;
 
 
+
 const Menu = () => {
     const [open, setOpen] = React.useState(false);
+    const links = document.querySelectorAll(".link");
 
     React.useEffect(() => {
+        const links = document.querySelectorAll(".link") as NodeListOf<Element>;
+
+        links.forEach((link, index) => {
+            link.addEventListener("mouseover", () => {
+                console.log("hover");
+                const bgElement = document.getElementById("bg") as HTMLImageElement;
+
+                // Ensure the image is loaded
+                if (!bgElement || !bgElement.complete) return;
+
+                // Calculate the correct Y position for the hovered link
+                const parts = links.length + 1;
+                const yOffset = -(bgElement.naturalHeight / parts) * (index);
+
+                // Animate the background image
+                gsap.to(bgElement, {
+                    duration: 1,
+                    ease: "power2.Out",
+                    y: yOffset + "px",
+                });
+            });
+        });
+    }, []);
+
+
+    React.useEffect(() => {
+        gsap.to(".head", {
+            opacity: 1,
+            duration: 1,
+
+            stagger: 0.1
+        })
     }, [open])
 
     return (
         <>
-            <div className={"w-full absolute top-0 z-[2] left-0 p-3 px-6 flex items-start"}>
+            <div className={"w-screen z-10 absolute top-0 z-2 left-0 px-8 p-4 flex items-center justify-center"}>
                 <div className={"w-1/2 flex items-center gap-x-8 justify-start"}>
-                    <Link href='/' className={"text-[20px] link opacity-0"}><span className={" font-[family-name:var(--font-ppuli)] text-[#B2F5A5]"}>Cami_</span>BC</Link>
-                    <Link href='https://g1anc.github.io' className="text-sm link opacity-0">Made by <I><span className="text-sm tracking-wider">Aven Inc.</span></I></Link>
+                    <Link href='/' className={"text-[20px] head opacity-0"}>
+                        <span className={" font-[family-name:var(--font-ppuli)] text-[#B2F5A5]"}>Cami_</span>BC
+                    </Link>
+                    <Link href='https://g1anc.github.io' className="text-sm head opacity-0">
+                        Made by <I><span className="text-sm tracking-wider">Aven Inc.</span></I>
+                    </Link>
                 </div>
                 <div className={"w-1/2 flex items-center gap-x-8 justify-end"}>
-                <button className="border rounded-full border-[#B2F5A5] p-2 px-4 text-[#B2F5A5] hover:bg-[#B2F5A5] hover:text-black duration-200 transition-all" style={{boxShadow: "0px 0px 10px #B2F5A590"}}>Menu</button>
-      
+                    <button className={`${!open && "border border-[#B2F5A5] hover:bg-[#B2F5A5] text-[16px] text-[#B2F5A5] hover:text-black"} hover:scale-110 head opacity-0 rounded-full p-3 px-4 duration-200 transition-all`}
+                            onClick={() => setOpen(!open)}
+                    >
+                        {!open ? "Menu" : <div className={""}><img width={24} height={24} src={'/cross.svg'} alt={"close"}/></div>}
+                    </button>
                 </div>
             </div>
-            <div className={`w-full h-full z-30 absolute top-0 left-0 ${open ? "flex" : "hidden"} flex-col items-center justify-center bg-[#B2F5A5]`}>
-                <Link href="/about" className="link">About</Link>
-                <Link href="/works" className="link">Works</Link>
-                <Link href="/contact" className="link">Contact</Link>
+
+            <div className={`w-full h-full z-[8] absolute top-0 left-0 ${open ? "flex" : "hidden"} bg-black`}>
+                <div className={"absolute bottom-4 z-[10] left-0 w-full h-auto flex"}>
+                    <div className={"pl-8 w-full text-start text-xl"}>+41 78 696 37 59</div>
+                    <div className={"pr-8 w-full text-end text-2xl"}>
+                        <div className={""}>+41 78 696 37 59</div>
+                        <div className={""}>camillebonnetcrevel@gmail.com</div>
+                    </div>
+                </div>
+                <div className={"absolute top-0 left-0 w-full h-full"}>
+                    <img src={"/bg.png"} className=" z-[-1] opacity-100 blur-3xl w-full bg object-contain absolute top-0 left-0" id={"bg"} alt={"bg"}>
+                    </img>
+                </div>
+                <div className={"w-full h-screen z-40 uppercase flex flex-col items-center gap-y-8 justify-center "}>
+                    <Link href="/" className="link flex items-center justify-center text-center ">Home</Link>
+                    <Link href="/works"
+                          className="link flex items-center justify-center text-center ">Works</Link>
+                    <Link href="/about"
+                          className="link flex items-center justify-center text-center  ">About</Link>
+                    <Link href="/contact"
+                          className="link flex items-center justify-center text-center font-[family-name:var(--font-ppuli)] text-[#B2F5A5]  capitalize" style={{textShadow: "0px 0px 10px #B2F5A5"}}>Contact</Link>
+                </div>
             </div>
         </>
-        
+
     )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@
 
 import React, { useEffect, useRef } from "react";
 import hoverEffect from "hover-effect";
-// import Image from "next/image";
-// import * as THREE from "three";
 import {gsap} from "gsap";
 
 import Menu from "./components/menu";
@@ -14,6 +12,7 @@ const I = ({children}: {children: React.ReactNode}) => <span className="font-[fa
 
 export default function Home() {
     const imageRef = useRef(null);
+
 
     useEffect(() => {
         const tl = gsap.timeline();
@@ -28,7 +27,7 @@ export default function Home() {
         });
 
         tl
-            .fromTo(".unlist", {x: "-200vw"}, {x: 0, opacity: 1, duration: 2, ease: "power2.inOut"})
+            .fromTo(".unlist", {x: "120vw"}, {x: 0, opacity: 1, duration: 2, ease: "power2.inOut"})
             .fromTo(".me", {y: "-100%"}, {y: 0, duration: 1.5, ease: "power4.out"}, "-=1")
             .fromTo(".welcome", {y: "100%"}, {y: 0, duration: 0.5, ease: "power.in", stagger: 0.1})
     }, []);
@@ -36,36 +35,56 @@ export default function Home() {
     return (
         <div className="h-screen w-screen relative overflow-hidden font-[family-name:var(--font-helvetica)]">
             <div className="w-full h-full absolute top-0 left-0">
-                <div className="w-full h-full" style={{
+                <div className="w-full h-full opacity-50" style={{
                     backgroundImage: "url(/bg.png)",
                     backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    filter: "blur(10px)",
+                    backgroundPosition: "top",
+                    filter: "blur(40px)",
                     zIndex: -1,
+                    opacity: "50%",
                 }}></div>
             </div>
-            <Header />
             <Menu />
             <div className="w-screen h-screen flex items-center justify-center gap-x-8 overflow-hidden">
                 <div className="w-full h-full flex justify-end">
                     <div className="w-2/3 h-full inset-0 relative overflow-hidden">
-                        <div ref={imageRef} className="me w-full h-full object-cover" />
+                        <div ref={imageRef} className="me w-full h-full object-cover"/>
                     </div>
                 </div>
 
                 <div className="flex flex-col items-start justify-center text-xl h-2/3 w-full">
-                    <div className="flex items-start flex-col justify-center gap-y-8">                    
-                        <div className="relative inset-0 overflow-hidden"><div className="welcome">Welcome to my <I> Portfolio</I> !</div></div>
-                        <div className="">
-                            <div className="relative inset-0 overflow-hidden"><div className="welcome">I’m a designer and photograph from <I>France </I>.</div></div>
-                            <div className="relative inset-0 overflow-hidden"><div className="welcome">I’m focused on  <I>(re)shaping</I>  the way you’re seen.</div></div>
-                            <div className="relative inset-0 overflow-hidden"><div className="welcome"></div></div>
-                            <div className="relative inset-0 overflow-hidden"><div className="welcome">Granting <I>elegancy</I> and <I>creativity</I></div></div>
-                            <div className="relative inset-0 overflow-hidden"><div className="welcome">into your branding and bringing it to <I>life</I>.</div> </div>
+                    <div className="flex items-start flex-col justify-center gap-y-8">
+                        <div className="relative inset-0 overflow-hidden">
+                            <div className="welcome">Welcome to my <I> Portfolio</I> !</div>
                         </div>
-                        <button className="border rounded-full border-[#B2F5A5] p-2 px-4 text-[#B2F5A5] hover:bg-[#B2F5A5] hover:text-black duration-200 transition-all" style={{boxShadow: "0px 0px 10px #B2F5A590"}}>Contact me</button>
+                        <div className="">
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome">I’m a designer and photograph from <I>France </I>.</div>
+                            </div>
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome">I’m focused on <I>(re)shaping</I> the way you’re seen.</div>
+                            </div>
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome"></div>
+                            </div>
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome">Granting <I>elegancy</I> and <I>creativity</I></div>
+                            </div>
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome">into your branding and bringing it to <I>life</I>.</div>
+                            </div>
+                            <div className="relative inset-0 overflow-hidden">
+                                <div className="welcome"></div>
+                            </div>
+                        </div>
+                        <div className={"relative inset-0 overflow-hidden"}>
+                            <button
+                                className="welcome border rounded-full border-[#B2F5A5] p-2 px-4 text-[#B2F5A5] hover:bg-[#B2F5A5] hover:text-black duration-200 transition-all">Contact me
+                            </button>
+
+                        </div>
                     </div>
-                    
+
                 </div>
                 <div className="w-full absolute  flex items-end z-[-3] text-2xl justify-center left-0 h-[50%]">
                     <div className="w-full h-auto flex items-center justify-center">
@@ -78,11 +97,11 @@ export default function Home() {
                     </div>
                 </div>
             
-                <div className="w-full absolute bottom-0 flex flex-col items-center  justify-center left-0 h-[28%] ">
+                <div className="w-full absolute bottom-0 flex flex-col items-center justify-center left-0 h-[28%] ">
                     <div className="relative h-full w-full overflow-hidden inset-0">
-                        <ul className="unlist opacity-0 list-none select-none absolute h-full inline-flex items-center">
+                        <ul className="unlist list-none select-none absolute h-full inline-flex items-center">
                             {Array.from({ length: 50 }).map((_, i) => (
-                                <li key={i} className="font-bold text-[250px] flex-shrink-0 pt-8 px-8 tracking-[-10px]">
+                                <li key={i} className="font-bold text-[250px] flex items-center justify-center flex-shrink-0 pt-32 h-full px-8 tracking-[-5px]">
                                     <span className="text-[#B2F5A5] text-[230px] mr-16 font-extralight tracking-normal font-[family-name:var(--font-ppuli)]" style={{textShadow: "0px 0px 10px #B2F5A5"}}>
                                         Camille
                                     </span>
